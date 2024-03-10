e2b67e19ce42d7f1b7493d3daeafd625e2d76d40
amiel103 <84434249+amiel103@users.noreply.github.com>
2024-03-11 01:49:12 +0800

    qwe

--- pos/settings.py
+++ pos/settings.py
@@ -143,7 +143,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # (
 #     # location of your application, should not be public web accessible 
 #     './static',
