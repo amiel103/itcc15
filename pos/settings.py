f10e2f733b0ab2ee85f568c82da2719ef3f926c0
amiel103 <84434249+amiel103@users.noreply.github.com>
2024-03-11 02:31:44 +0800

    try change static

--- pos/settings.py
+++ pos/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
 STORAGES = {
     # ...
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 # (
