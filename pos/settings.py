d011a5f7720dfbf796619f8a868f737430ae303c
amiel103 <84434249+amiel103@users.noreply.github.com>
2024-03-11 17:14:21 +0800

    Update settings.py

--- pos/settings.py
+++ pos/settings.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
+    "django.contrib.staticfiles",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
